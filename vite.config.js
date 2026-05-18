dbc612cc1bdaf2403f266b131771608fd35675bb
김민조 <mjkim@tricycle.co.kr>
2026-05-18 16:14:27 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/wedding/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    base: "/wedding/",
   },
 });
